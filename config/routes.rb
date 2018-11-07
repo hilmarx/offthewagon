eca8b54b7fe1d5ba3cf7d214e7cf5283279e4741
Hilmar Orth <hm.orth@aol.com>
2018-11-07 17:16:01 +0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+git remote add origin git@github.com:hilmarx/offthewagon.git
+git push origin master
