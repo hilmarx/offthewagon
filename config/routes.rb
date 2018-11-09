d9df8357fd5773218def75eb9f0e25e0e098b026
william oleson <wnoleson@gmail.com>
2018-11-09 13:47:31 +0800

    controller, routes, routing buttons to teach for new booking, and teacher index to display bookings

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
   resources 'bookings', only:[:new, :create, :index, :show, :edit, :update, :destroy]
 
   resources 'teachers', only:[:index, :show, :edit, :update, :new, :create] do
+     resources 'bookings', only:[:new, :create, :destroy]
     # As a user, I want to see all my bookings create a new booking and delete it if I no longer want the session
     # As a teacher, I want to see all my bookings, edit their status and delete them
-    resources 'bookings', only:[:destroy]
     # As a teacher, I want to show all my skills, add new skills and delete skills I no longer possess
     resources 'teacher_skills', only:[:index, :new, :create, :destroy]
   end
