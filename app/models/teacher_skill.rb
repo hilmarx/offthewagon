f763a351904170fee50674d40a4d4470b4a44dc6
Hilmar X <hm.orth@aol.com>
2018-11-12 14:54:40 +0800

    Update teacher_skill.rb

--- app/models/teacher_skill.rb
+++ app/models/teacher_skill.rb
@@ -1,14 +1,5 @@
 class TeacherSkill < ApplicationRecord
   belongs_to :skill
   belongs_to :teacher
-  validates :skill_id, uniqueness: true
-  # after_initialize :set_name
 
-
-  # def set_name
-  #   @skill =
-  # end
-
-  # Elastic Search
-  # searchkick
 end
