e13c081e8e05830e370b844ded723f6b7a228983
Hilmar X <hm.orth@aol.com>
2018-11-12 14:52:50 +0800

    Update booking.rb

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,11 +1,4 @@
 class Booking < ApplicationRecord
   belongs_to :teacher
   belongs_to :user
-  # before_save :no_self_booking
-
-
-  # def no_self_booking
-  #   self.user_id != Teacher.find(teacher_id).user_id
-  # end
-
 end
