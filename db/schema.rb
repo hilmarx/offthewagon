005c44f861fd69a5523b5fd6b4801d99b7aa3933
Jethro Williams <jethrowilliams4@hotmail.com>
2018-11-12 12:40:46 +0800

    Add an admin column to users, with a default value false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_11_030855) do
+ActiveRecord::Schema.define(version: 2018_11_12_043744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2018_11_11_030855) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
