2137bb4955d068b3c982962dba7520a1b7764db5
Hilmar Orth <hm.orth@aol.com>
2018-11-12 12:17:21 +0800

    added validation that user_id in Teacher class must be unique

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -3,13 +3,15 @@ class Teacher < ApplicationRecord
   has_many :teacher_skills
   has_many :skills, through: :teacher_skills
   has_many :bookings
+  validates :user_id, uniqueness: true
 
   # Pg Search Start
+  # Search by Teacher.user name and skill category
   include PgSearch
   pg_search_scope :search_by_teacher_user_name,
     against: [ :title, :syllabus ],
     associated_against: {
-      director: [ :first_name, :last_name ]
+      user: [ :first_name, :last_name ]
     },
     using: {
       tsearch: { prefix: true }
