582391c5808c1647ac147accd701b413aca1f326
Hilmar Orth <hm.orth@aol.com>
2018-11-13 14:55:05 +0800

    fixed validation Skills

--- app/models/teacher_skill.rb
+++ app/models/teacher_skill.rb
@@ -3,6 +3,7 @@ class TeacherSkill < ApplicationRecord
   belongs_to :teacher
   before_save :set_name
   validates :level, inclusion: [1, 2, 3, 4, 5]
+  validates_uniqueness_of :teacher_id, :scope => [:skill_id]
 
 
   def set_name
